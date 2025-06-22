bd4dae79ce7b4d1a3dd70439daf14afe6a78e0c9
LYGreen <3098036603@qq.com>
2025-06-22 14:25:09 +0800

    更改创建时间和修改时间的显示格式

--- .vitepress/theme/component/Article.vue
+++ .vitepress/theme/component/Article.vue
@@ -19,7 +19,8 @@ onMounted(() => {
 
 function formatISOString(isoString) {
     const date = new Date(isoString);
-    return `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`
+    return `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} ` +
+        `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
 }
 
 </script>
