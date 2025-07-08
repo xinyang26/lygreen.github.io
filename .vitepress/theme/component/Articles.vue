c66dd45bf0770becbfbe49c57b923e5f6bce1a32
LYGreen <3098036603@qq.com>
2025-07-08 23:53:50 +0800

    修复链接

--- .vitepress/theme/component/Articles.vue
+++ .vitepress/theme/component/Articles.vue
@@ -51,12 +51,12 @@ function getRandomInt(min, max) {
             </a>
             <div class="bottom">
                 <div class="category">
-                    <a :href="'#'" v-for="(e, i) in item.category" v-bind:key="i">
+                    <a :href="base + 'category/' + e" v-for="(e, i) in item.category" v-bind:key="i">
                         {{ e }}
                     </a>
                 </div>
                 <div class="tag">
-                    <a :href="'#'" v-for="(e, i) in item.tag" v-bind:key="i">
+                    <a :href="base + 'tag/' + e" v-for="(e, i) in item.tag" v-bind:key="i">
                         {{ e }}
                     </a>
                 </div>
